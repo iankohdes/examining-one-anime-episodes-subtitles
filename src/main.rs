d8ecb6ae8df05fcbf63069949229a34ffcb2b6e8
Ian Koh <29516989+iankohdes@users.noreply.github.com>
2025-06-15 22:52:21 +0200

    Update main.rs with meaningful temp code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    /*
+    The bracket that doesn’t take up the space of an imaginary square is used
+    for kana characters, to help with pronouncing kanji.
+     */
+    let left1 = '（' as u32;
+    let left2 = '(' as u32;
+    println!("（ as u32: {left1}\n( as u32: {left2}\n");
+
+    /*
+    Small hiragana from the a-column (i.e. consonants paired with the ‘a’ sound
+    are long vowel markers, similar to ー.
+     */
+    let small_a = 'ぁ' as u32;
+    let big_a = 'あ' as u32;
+    println!("Small ぁ: {small_a}\nBig あ: {big_a}");
 }
