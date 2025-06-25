3c620881c4c310d212b6a35a72bc600f2ac01ac0
Ian Koh <29516989+iankohdes@users.noreply.github.com>
2025-06-25 08:58:10 +0200

    Create a const variable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#![allow(dead_code)]
+
 fn main() {
     todo!()
 }
+
+struct FilePath(String);
+
+const fn create_filepath(path: String) -> FilePath {
+    FilePath(path)
+}
+
+const MINI_KANA_JSON_PATH: FilePath = create_filepath(String::from("data/raw/mini_kana_mappings.json"));
