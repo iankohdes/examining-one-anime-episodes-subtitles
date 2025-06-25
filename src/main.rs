c8799c7afb9d12bca6a09bebea86ad4e6e906209
Ian <29516989+iankohdes@users.noreply.github.com>
2025-06-25 17:17:38 +0200

    Add internal documentation to code

--- src/main.rs
+++ src/main.rs
@@ -8,25 +8,46 @@ use std::io::BufReader;
 use serde::Deserialize;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let io_test_input = File::open(String::from("data/raw/mini_kana_mappings.json"))?;
-    let test_buffer: BufReader<File> = BufReader::new(io_test_input);
-    let test_hash_map: HashMap<SmallKana, RegularKana> = serde_json::from_reader(test_buffer)?;
+    let mini_kana_mappings = ingest_mini_kana_mappings()?;
+    let test_string = convert_mini_kana_to_regular(&"こうなっちまったら最後―", &mini_kana_mappings);
 
-    let test_string = convert_mini_kana_to_regular(&"こうなっちまったら最後―", &test_hash_map);
     println!("こうなっちまったら最後―");
     println!("{test_string}");
 
     Ok(())
 }
 
+/// Path to a JSON file containing the regular-to-small kana mappings for 10
+/// different sounds: _a_, _i_, _u_, _e_, _o_, _tsu_, _ya_, _yu_, _yo_, and _wa_.
+/// These sounds are represented by both **hiragana** and **katakana**
+/// syllabaries.
 const MINI_KANA_JSON_PATH: &'static str = "data/raw/mini_kana_mappings.json";
 
 #[derive(Deserialize, Eq, PartialEq, Hash, Debug)]
+/// Newtype representing the **keys** in `mini_kana_mappings.json`.
 struct SmallKana(char);
+/// Newtype representing the _values_ in `mini_kana_mappings.json`.
 #[derive(Deserialize, Eq, PartialEq, Hash, Debug)]
 struct RegularKana(char);
 
 fn ingest_mini_kana_mappings() -> Result<HashMap<SmallKana, RegularKana>, Box<dyn std::error::Error>> {
+    //! Takes no arguments and returns a `Result` of `HashMap<SmallKana,
+    //! RegularKana>` in the successful case and `Box<dyn std::error::Error>`
+    //! otherwise.
+    //!
+    //! The reason that `ingest_mini_kana_mappings` does not take an argument is
+    //! that it has only one possible input value: the `MINI_KANA_JSON_PATH`.
+    //!
+    //! Usage example:
+    //!
+    //! ```
+    //! fn main() -> Result<(), Box<dyn std::error::Error>> {
+    //!     let mini_kana_mappings = ingest_mini_kana_mappings()?;
+    //!     println!("{mini_kana_mappings:?}");
+    //!     Ok(())
+    //! }
+    //! ```
+
     let io_file = File::open(MINI_KANA_JSON_PATH)?;
     let buffer: BufReader<File> = BufReader::new(io_file);
     let mini_kana_mappings: HashMap<SmallKana, RegularKana> = serde_json::from_reader(buffer)?;
@@ -37,16 +58,12 @@ fn convert_mini_kana_to_regular(
     input: &str,
     kana_mapping: &HashMap<SmallKana, RegularKana>
 ) -> String {
-    /*
-    [DONE] First, there should be a function that operates on the character level. It takes
-    one character, checks to see if it’s in the kana_mapping hash map, then returns
-    some output.
-
-    [DONE] Next, there should be a function that maps mini_kana_character_to_regular to every
-    character in the input string, converting small kana to regular.
-    
-    Finally, move the code over to cleaning.rs.
-     */
+    //! **String-level** cleaning function that maps `mini_kana_character_to_regular`
+    //! to every `char` in the input.
+    //! 
+    //! Use this together with the `ingest_mini_kana_mappings` function to create
+    //! the hash map that will be used as the second argument. The hash map is
+    //! also passed to `mini_kana_character_to_regular` as a parameter.
     let converted_input: String = input
         .chars()
         .map(|x| mini_kana_character_to_regular(&x, kana_mapping))
@@ -59,6 +76,13 @@ fn mini_kana_character_to_regular(
     input: &char,
     kana_mapping: &HashMap<SmallKana, RegularKana>
 ) -> char {
+    //! **Character-level** cleaning function that transforms a kana character
+    //! into its regular size if it’s found to be a small version, and returns
+    //! the input unchanged otherwise.
+    //!
+    //! Small kana versions indicate digraphs, lengthened vowels or the presence
+    //! of double consonants.
+
     let typed_input = SmallKana(*input);
 
     let unwrapped_output: char = match kana_mapping.get(&typed_input) {
