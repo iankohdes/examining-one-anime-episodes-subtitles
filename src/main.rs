81d3bbf22a9e679733489c6a6188f135d1a9c3f3
Ian Koh <29516989+iankohdes@users.noreply.github.com>
2025-06-26 21:45:08 +0200

    Write deduping and sorting helper function + organise code

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,51 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
 
-use std::collections::HashMap;
+mod dataprep;
+
+use dataprep::cleaning::helper_dedupe_and_sort;
+use std::collections::{BTreeSet, HashMap};
+use std::fs;
 use std::fs::File;
 use std::io::BufReader;
 use serde::Deserialize;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mini_kana_mappings = ingest_mini_kana_mappings()?;
-    let test_string = convert_mini_kana_to_regular(&"こうなっちまったら最後―", &mini_kana_mappings);
+    // let mini_kana_mappings = ingest_mini_kana_mappings()?;
+    // let test_string = convert_mini_kana_to_regular(&"こうなっちまったら最後―", &mini_kana_mappings);
+    // let test_string_2 = "えっと… あの どうすれば？サイコパ99スを読み取る銃だ―australiaセーフティーが 解除される―…";
+    //
+    // let deduped_and_sorted: String = test_string_2
+    //     .chars()
+    //     .collect::<BTreeSet<_>>()
+    //     .into_iter()
+    //     .collect();
+    //
+    // println!("こうなっちまったら最後―");
+    // println!("{test_string_2}");
+    // println!("{deduped_and_sorted}");
 
-    println!("こうなっちまったら最後―");
-    println!("{test_string}");
+    let raw_content: String = fs::read_to_string("data/raw/psycho-pass-s01e01-jp.srt")?;
+    let normalised_raw_content: String = raw_content.replace("\r\n", "\n");
+
+    let raw_content_split: Vec<&str> = split_into_raw_subtitle_units(&normalised_raw_content);
+    let subtitles: Vec<&str> = raw_content_split
+        .iter()
+        .flat_map(|x| get_subtitles_from_unit(x))
+        .collect();
+    let subtitles_concat = subtitles.join("");
+
+    helper_dedupe_and_sort(&subtitles_concat);
 
     Ok(())
+}
+
+fn split_into_raw_subtitle_units(raw: &str) -> Vec<&str> {
+    raw.split("\n\n").collect()
+}
+
+fn get_subtitles_from_unit(subtitle_unit: &str) -> Vec<&str> {
+    subtitle_unit.split('\n').skip(2).collect()
 }
 
 /// Path to a JSON file containing the regular-to-small kana mappings for 10
