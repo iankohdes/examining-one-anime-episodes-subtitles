22b6772056571c66220f3323aff3ac8d76e22cc1
Ian Koh <29516989+iankohdes@users.noreply.github.com>
2025-06-26 21:45:08 +0200

    Write deduping and sorting helper function + organise code

---
+++ src/dataprep/cleaning.rs
@@ -0,0 +1,30 @@
+use std::collections::BTreeSet;
+
+pub fn helper_dedupe_and_sort(xs: &str) {
+    //! Deduplicates a string and sorts its characters, then **prints the result**.
+    //!
+    //! Useful for identifying unwanted characters to add to a blacklist. Uses
+    //! the `BTreeSet` data structure.
+    //!
+    //! Unwanted characters are defined as follows:
+    //!
+    //! - Punctuation and spaces
+    //! - Latin or non-Japanese characters
+    //! - [_Chōonpu_](https://en.wikipedia.org/wiki/Ch%C5%8Donpu) (a symbol that
+    //!   indicates the prolonged sound of the katakana character immediately
+    //!   preceding it)
+    //!
+    //! _Chōonpu_ usually come after katakana characters and rarely after
+    //! hiragana ones.
+    //!
+    //! The point of sorting is to (hopefully!) cluster unwanted characters
+    //! away from kanji, hiragana and katakana.
+
+    let deduped_and_sorted: String = xs
+        .chars()
+        .collect::<BTreeSet<_>>()
+        .into_iter()
+        .collect();
+
+    println!("{deduped_and_sorted}");
+}
