00d77b950221bd2413efe87ff11282a100a7ecab
Ian Koh <29516989+iankohdes@users.noreply.github.com>
2025-07-04 09:01:10 +0200

    Write ingestion function for subtitle file

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,14 @@ use std::fs;
 use std::fs::File;
 use std::io::BufReader;
 use serde::Deserialize;
+use crate::dataprep::ingestion::ingest_subtitle_file;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let raw_content: String = fs::read_to_string("data/raw/psycho-pass-s01e01-jp.srt")?;
-    let normalised_raw_content: String = raw_content.replace("\r\n", "\n");
+    let ingested_subtitles =
+        ingest_subtitle_file("data/raw/psycho-pass-s01e01-jp.srt")?;
+    let cleaned_subtitles = clean_subtitles(&ingested_subtitles)?;
 
-    let raw_content_split: Vec<&str> = split_into_raw_subtitle_units(&normalised_raw_content);
-    let subtitles: Vec<&str> = raw_content_split
-        .iter()
-        .flat_map(|x| get_subtitles_from_unit(x))
-        .collect();
-    let subtitles_concat = subtitles.join("");
-
-    let cleaned_subtitles = clean_subtitles(&subtitles_concat)?;
     println!("{cleaned_subtitles}");
 
     Ok(())
-}
-
-fn split_into_raw_subtitle_units(raw: &str) -> Vec<&str> {
-    raw.split("\n\n").collect()
-}
-
-fn get_subtitles_from_unit(subtitle_unit: &str) -> Vec<&str> {
-    subtitle_unit.split('\n').skip(2).collect()
 }
