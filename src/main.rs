292c6d895c97ec4862a3c4a616b1e73bd43efea8
Ian Koh <29516989+iankohdes@users.noreply.github.com>
2025-12-04 01:39:32 +0100

    Formatting changes by Zed IDE

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,14 @@ mod dataprep;
 
 use dataprep::cleaning::{clean_subtitles, helper_dedupe_and_sort};
 use dataprep::ingestion::ingest_subtitle_file;
+use serde::Deserialize;
 use std::collections::{BTreeSet, HashMap};
 use std::fs;
 use std::fs::File;
 use std::io::BufReader;
-use serde::Deserialize;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let ingested_subtitles =
-        ingest_subtitle_file("data/raw/psycho-pass-s01e01-jp.srt")?;
+    let ingested_subtitles = ingest_subtitle_file("data/raw/psycho-pass-s01e01-jp.srt")?;
     let cleaned_subtitles = clean_subtitles(&ingested_subtitles)?;
 
     println!("{cleaned_subtitles}");
