cd914b3b34e8ba509a9613f1f5757a64f08f7b46
Ian Koh <29516989+iankohdes@users.noreply.github.com>
2025-12-13 19:06:50 +0100

    Housekeeping prep for new changes

--- src/dataprep/subtitle_set_builder.rs
+++ src/dataprep/subtitle_set_builder.rs
@@ -23,8 +23,9 @@ pub struct Timing {
     pub end: Timestamp,
 }
 
-/// Rich timestamp representation.
-/// Represents `hh:mm:ss,mmm`.
+///Rich timestamp representation.
+///Represents `hh:mm:ss,mmm`.
+// Implement the TryFrom trait
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Timestamp {
     pub hours: u8,
@@ -37,35 +38,6 @@ pub struct Timestamp {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct SubtitleSet {
     pub units: Vec<SubtitleUnit>,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct RawSubtitleUnit {
-    raw_index: String,
-    raw_timing: String,
-    raw_lines: Vec<String>,
-}
-
-impl TryFrom<&str> for RawSubtitleUnit {
-    type Error = &'static str;
-
-    // ["", "266", "00:18:25,437 --> 00:18:27,439", "そのための", "パラライザーだろうが！―"]
-    fn try_from(s: &str) -> Result<Self, Self::Error> {
-        if s.trim().is_empty() {
-            return Err("RawSubtitleUnit cannot be empty");
-        } else if s.contains("\n\n") {
-            return Err("RawSubtitleUnit cannot contain empty newline")
-        }
-
-        let s_split: Vec<&str> = s.split("\n").map(|s| s.trim()).collect();
-        let s_split_cleaned: Vec<&str> = remove_empty_first_element(s_split);
-
-        let raw_index = s_split_cleaned[0].to_string();
-        let raw_timing = s_split_cleaned[1].to_string();
-        let raw_lines = s_split_cleaned[2..].iter().map(|&s| s.to_string()).collect();
-
-        Ok(RawSubtitleUnit { raw_index, raw_timing, raw_lines })
-    }
 }
 
 fn remove_empty_first_element(input: Vec<&str>) -> Vec<&str> {
