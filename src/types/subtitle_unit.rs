2afb5ae9b2757ab2ac756293a5ae8d1c15196a85
Ian Koh <29516989+iankohdes@users.noreply.github.com>
2026-03-10 01:09:57 +0100

    Add subtitle unit example format from subtitle_set_builder.rs
    Reset author info

--- src/types/subtitle_unit.rs
+++ src/types/subtitle_unit.rs
@@ -8,7 +8,15 @@ use crate::types::timing::Timing;
 /// respectively. All successive lines are the subtitle content.
 ///
 /// An SRT file, after parsing, consists of a collection of `SubtitleUnit`
-/// elements.
+/// elements. See below an example of a subtitle block.
+///
+/// ```text
+/// 12
+/// 00:01:02,510 --> 00:01:04,120
+/// Hello world!
+/// Potential second line
+/// ```
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct SubtitleUnit {
     pub index: SrtIndex,
     pub timing: Timing,
