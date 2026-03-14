15b278b73f8c3c611518f4182626cd5888db9a49
Ian Koh <29516989+iankohdes@users.noreply.github.com>
2026-03-14 20:36:04 +0100

    Return correct value after loop termination

--- src/dataprep/parser_alt.rs
+++ src/dataprep/parser_alt.rs
@@ -1,3 +1,4 @@
+use std::cmp::PartialEq;
 use std::fmt::Display;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -7,19 +8,21 @@ use crate::types::subtitle_unit::SubtitleUnit;
 use crate::types::timing::TimingError;
 use crate::types::parse_state::ParseState;
 
+#[derive(Debug, Clone, PartialEq, Eq)]
 struct SrtParser {
     output: Vec<SubtitleUnit>
 }
 
+#[derive(Debug, PartialEq, Eq)]
 enum SrtParserError {
-    Io(std::io::Error),
+    Io(String),
     SrtIndexError(SrtIndexError),
     TimingError(TimingError),
     SubtitleParserError(String)
 }
 
 impl SrtParser {
-    pub fn parse(reader: BufReader<File>) -> Result<SrtParser, SrtParserError> {
+    pub fn parse(reader: BufReader<File>) -> Result<Vec<SubtitleUnit>, SrtParserError> {
         let mut output: Vec<SubtitleUnit> = Vec::new();
         let mut current_state: ParseState = ParseState::Empty;
 
@@ -28,9 +31,8 @@ impl SrtParser {
 
         loop {
             if current_line.is_none() {
-                break;
-            }
-            else if current_line.unwrap().unwrap() == "" && current_state == ParseState::Empty {
+                return Ok(output);
+            } else if current_line.unwrap().unwrap() == "" && current_state == ParseState::Empty {
                 current_line = raw_iter.next();
             } else if current_line.is_some() && current_state == ParseState::Empty {
                 let srt_index = current_line.unwrap().unwrap().parse::<SrtIndex>().unwrap();
