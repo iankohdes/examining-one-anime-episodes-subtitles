d702b5f9e57ba66aa6b6f928a96064d2036d6f92
Ian Koh <29516989+iankohdes@users.noreply.github.com>
2026-03-14 17:59:14 +0100

    Implement parser with state machine (unsuccessfully)

--- src/dataprep/parser_alt.rs
+++ src/dataprep/parser_alt.rs
@@ -1,12 +1,14 @@
 use std::fmt::Display;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufRead, BufReader};
 use crate::dataprep::parser::SubtitleParserError;
-use crate::types::srt_index::SrtIndexError;
+use crate::types::srt_index::{SrtIndex, SrtIndexError};
+use crate::types::subtitle_unit::SubtitleUnit;
 use crate::types::timing::TimingError;
+use crate::types::parse_state::ParseState;
 
 struct SrtParser {
-    parse_object: BufReader<File>
+    output: Vec<SubtitleUnit>
 }
 
 enum SrtParserError {
@@ -14,4 +16,22 @@ enum SrtParserError {
     SrtIndexError(SrtIndexError),
     TimingError(TimingError),
     SubtitleParserError(String)
+}
+
+impl SrtParser {
+    pub fn parse(reader: BufReader<File>) -> Result<SrtParser, SrtParserError> {
+        let mut output: Vec<SubtitleUnit> = Vec::new();
+        let mut current_state: ParseState = ParseState::Empty;
+
+        let mut raw_iter = reader.lines();
+        let mut current_line = raw_iter.next();
+
+        loop {
+            if current_line.unwrap().unwrap() == "" && current_state == ParseState::Empty {
+                current_line = raw_iter.next();
+            } else if current_line.is_some() && current_state == ParseState::Empty {
+                let srt_index = current_line.unwrap().unwrap().parse::<SrtIndex>().unwrap();
+            }
+        }
+    }
 }
